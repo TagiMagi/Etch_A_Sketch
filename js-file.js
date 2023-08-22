03154d874775ab10d1db31a471ea0c38d03707d5
Tagi Magi <134989360+TagiMagi@users.noreply.github.com>
2023-08-22 10:32:17 -0500

    maintain area dimensions while accounting for input grid size

--- js-file.js
+++ js-file.js
@@ -1,26 +1,47 @@
 
 const container = document.querySelector('.container')
+
 let gridSize = 16;
 
-function gridSizePrompt () {
-    let el = document.getElementById('button')
-    gridSize = prompt("Enter a number under 100 to adjust the grid size", 16)
+// propmpt user for grid size 
 
+function gridSizePrompt () {
+    document.querySelector('.container').innerHTML = ""; 
+    
+    gridSize = Number(prompt("Enter a number under 100 to adjust the grid size", 16));
+    
+    makeGrid();
 }
+// create function that makes grid wuth user input while adjusting div height and width accordigly.
 
 function makeGrid() {
 
-for (let i = 0; i < gridSize; i++) {
+let styleHeight = 36;
+let styleWidth = 36;
+let border = 0;
+
+styleWidth = 576  / gridSize;
+styleHeight = styleWidth;
+
+
+    for (let i = 0; i < gridSize; i++) {
     let div = document.createElement('div');
     div.setAttribute('id', 'outerDivs');
     for (let j = 0; j < gridSize; j++) {
         let innerDiv = document.createElement('div')
-        innerDiv.setAttribute('id', 'innerDivs')
+        innerDiv.style.height = `${styleHeight}px`;
+        innerDiv.style.width = `${styleWidth}px`; 
+        innerDiv.setAttribute('id', 'innerDivs');
         div.appendChild(innerDiv);
     }
     container.appendChild(div);
 }
+
+let litBoxes = Array.from(document.querySelectorAll('#innerDivs'));
+litBoxes.forEach(div => div.addEventListener('mouseover', hoverLight))
+litBoxes.forEach(div => div.addEventListener('mouseout', hoverLightOff))
 }
+// two functions for the 'hover' effects 
 
 function hoverLight (e) {
     e.target.classList.add('overLight');
@@ -32,9 +53,8 @@ function hoverLightOff (e) {
 }
 
 window.onload = gridSizePrompt();
-makeGrid();
 
-let litBoxes = Array.from(document.querySelectorAll('#innerDivs'));
-//let litBoxes = document.getElementsById('innerDivs');
-litBoxes.forEach(div => div.addEventListener('mouseover', hoverLight))
-litBoxes.forEach(div => div.addEventListener('mouseout', hoverLightOff))
+let gridSizeButton = document.querySelector('button');
+gridSizeButton.addEventListener('click', gridSizePrompt);
+
+
